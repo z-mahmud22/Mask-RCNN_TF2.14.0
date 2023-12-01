b1591680fd1d1f0413916db5a9749590b3afc199
Zunayed Mahmud <107276459+z-mahmud22@users.noreply.github.com>
2023-12-01 10:24:14 -0500

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -1,1 +1,8 @@
+"""
+Mask R-CNN
+The main Mask R-CNN model implementation.
 
+Copyright (c) 2017 Matterport, Inc.
+Licensed under the MIT License (see LICENSE for details)
+Written by Waleed Abdulla
+"""
